e151f073259249b24a98e148551fd79ea1845c30
Noland Seigler <nolandseigler@gmail.com>
2020-05-31 22:55:24 -0500

    route variables practice

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ def secret():
     return "This is the /secret route"
 
 
+# this is not a query
+# this is a route param like /cat
+
+@app.route("/<string:variable>/<int:int_variable>")
+def variable_example(variable, int_variable):
+    return "This is the variable " + variable + ". This is the int variable " + str(int_variable)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
